f95357d1848bc0ec862011d0678486810ca3bda5
fish <yxzjerryfish@hotmail.com>
2019-10-24 21:39:21 +0800

    添加俩个模块对基础接口模块的依赖

---
+++ service/src/main/java/fish/service/Test.java
@@ -0,0 +1,14 @@
+package fish.service;
+
+/**
+ * 给包一个main方法
+ *
+ * @author : Fish Paradise
+ * @version 1.0
+ * @date : 2019/10/24 21:37
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
