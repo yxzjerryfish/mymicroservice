d49fdebafff407acb3c0e5bb9996c3bd9f4b2c90
fish <yxzjerryfish@hotmail.com>
2019-11-04 21:16:50 +0800

    流操作

--- service/src/main/java/fish/service/Test.java
+++ service/src/main/java/fish/service/Test.java
@@ -2,9 +2,11 @@ package fish.service;
 
 import fish.service.model.Dog;
 
+import java.util.Arrays;
 import java.util.function.BiConsumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
 /**
  * 给包一个main方法
@@ -23,5 +25,12 @@ public class Test {
         Function<Integer,Function<Integer,Function<Integer,Integer>>> fun = x->y->z->x+y+z;
 
         System.out.println(fun.apply(3).apply(4).apply(5));
+
+        int []a ={3,2,1,4,5};
+        Arrays.stream(a).filter(n->n%2==0).forEach(System.out::println);
+
+        IntStream.range(2,5).forEach(System.out::println);
+
+        IntStream.of(a).filter(n->n%2 != 0).map(n -> n*2).sorted().forEach(System.out::println);
     }
 }
