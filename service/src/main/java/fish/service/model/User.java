d6d9d811c71a630377eeee74dfab031b08e876e6
fish <yxzjerryfish@hotmail.com>
2019-11-09 22:24:00 +0800

    添加了swagger 描述

--- service/src/main/java/fish/service/model/User.java
+++ service/src/main/java/fish/service/model/User.java
@@ -1,7 +1,10 @@
 package fish.service.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * 用户
@@ -12,8 +15,16 @@ import lombok.Data;
  */
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@ApiModel(description = "用户模型")
 public class User {
+
+    @ApiModelProperty("姓名")
     String name;
+
+    @ApiModelProperty("年龄")
     Integer age;
+
+    @ApiModelProperty("描述")
     String desc;
 }
