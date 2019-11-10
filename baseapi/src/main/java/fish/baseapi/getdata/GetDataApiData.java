aa9c0858a864f37d3562c27e31cd792fee0dd434
fish <yxzjerryfish@hotmail.com>
2019-11-10 19:58:50 +0800

    继续添加swagger

--- baseapi/src/main/java/fish/baseapi/getdata/GetDataApiData.java
+++ baseapi/src/main/java/fish/baseapi/getdata/GetDataApiData.java
@@ -1,6 +1,8 @@
 package fish.baseapi.getdata;
 
 import fish.service.model.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,20 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
  * @date : 2019/10/27 22:16
  */
 @RestController
+@Api(value = "测试接口",tags = {"测试feign传送过来的值"})
 public class GetDataApiData {
     @Autowired
     TestReciveData testReciveData;
 
     @RequestMapping("/base/hello")
+    @ApiOperation(value = "通过feign获取hello")
     public String getHello(){
         return testReciveData.hello();
     }
 
+    @ApiOperation(value = "通过feign sayHello")
     @RequestMapping("/base/hello/{id}")
     public String getHello(@PathVariable String id){
         return testReciveData.sayhello(id);
     }
 
+    @ApiOperation(value = "通过feign调用无参hello")
     @RequestMapping("/base/hellofish")
     public User getHelloFish(){
         return testReciveData.sayHello();
