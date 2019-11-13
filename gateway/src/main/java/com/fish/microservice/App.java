deef823da1c19572df131e210efe06129e43792c
Fish Paradise <yxzjerryfish@hotmail.com>
2019-11-13 21:19:50 +0800

    将zuul注册到eureka

--- gateway/src/main/java/com/fish/microservice/App.java
+++ gateway/src/main/java/com/fish/microservice/App.java
@@ -1,13 +1,19 @@
 package com.fish.microservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 /**
  * Hello world!
  *
  */
+@EnableDiscoveryClient
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
