d66f822429c8fa7008059f41a7d17669f8155368
yxzjerryfish <yxzjerryfish@hotmail.com>
2020-01-04 17:09:31 +0800

    序列化及反序列化方法

---
+++ nettyweb/src/main/java/com/fish/microservice/util/JsonSerializerUtil.java
@@ -0,0 +1,22 @@
+package com.fish.microservice.util;
+
+import com.alibaba.fastjson.JSON;
+
+/**
+ * JSON序列化和反序列化
+ *
+ * @author : Fish Paradise
+ * @version 1.0
+ * @date : 2020/1/4 16:59
+ */
+public class JsonSerializerUtil {
+    public static byte[] serialize(Object object) {
+
+        return JSON.toJSONBytes(object);
+    }
+
+    public static <T>  T deserialize(Class<T> clazz, byte[] bytes) {
+
+        return JSON.parseObject(bytes, clazz);
+    }
+}
